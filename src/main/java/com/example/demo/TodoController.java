5bdd22dd33a40d5b08b51510ed11e62a32f7a4c9
jhs512 <jangka512@gmail.com>
2023-07-05 16:41:16 +0900

    삭제

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,5 +21,12 @@ public class TodoController {
         model.addAttribute("todos", todos);
 
         return "list";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable long id) {
+        todoService.delete(id);
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/demo/TodoService.java
+++ src/main/java/com/example/demo/TodoService.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -16,5 +17,11 @@ public class TodoService {
 
     public Todo save(Todo buyMilk) {
         return todoRepository.save(buyMilk);
+    }
+
+    public void delete(long id) {
+        Todo todo = todoRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+        todo.setDeleteDate(LocalDateTime.now());
+        todoRepository.save(todo);
     }
 }
